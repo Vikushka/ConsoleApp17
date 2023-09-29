731bbc48655474e3f7f691b5bf23af3f189e7bcf
R2 <R2@MILA-SHKA>
2023-09-29 21:40:18 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,16 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+ 
+
+var client = new RestClient("https://mfc.samgk.ru");
+var request = new RestRequest("api/groups");
+var response = client.ExecuteGet(request);
+Console.WriteLine(response.Content );
+var data = JsonSerializer.Deserialize<List<Group>>(response.Content!)!;
+
+
+
---
+++ ConsoleApp17/group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    public class Group
+    {
+       public int id;
+       public string name;
+       public string currator;
+
+    }
+}
